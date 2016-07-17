8d980af3bd6fecbc6ad5c0343859b3ddf4f7dfcd
Ben Harris <bdharris08@gmail.com>
2016-07-17 16:15:28 -0500

    Rewrote Singleton brutish
    Using for loop rather than foreach because I need to operate on the list
    and index anyway. Brutish is worst case O(n*n)

--- js/app/puzzles/singleton.js
+++ js/app/puzzles/singleton.js
@@ -12,43 +12,48 @@
 const assert = require('assert');
 
 const tBase = [0,0,1,1,2,2,3];
-const aBase = [3];
+const aBase = 3;
 const tUnordered = [0,3,1,2,0,3,2];
-const aUnordered = [1];
-const tDuplicate = [0,1,1,2,2,3];
-const aDuplicate = [0,3];
+const aUnordered = 1;
 const tTypes = [0, 'a', 'a', 0, "string"];
-const aTypes =  ["string"];
+const aTypes =  "string";
+const tDuplicate = [0,1,1,2,2,3];//exception
 
-
-
+//loops through each element, checking that element against the input with itself removed in order
+//worst case O(n^2)
 function findSingletonBrutish (input) {
-	const inputSize = input.length;
 	var returnThis = [];
-	input.forEach(function(item, index) {
-		const element = item;
-		//some returns true as soon as any of the callbacks return true
-		input.some(function(subItem,index) { 
-			if (index >= inputSize){
-				returnThis.push(element);
-			} else {
-				if (element === subItem) {
-					return true;
-				}
+	for (var i = 0; i < input.length; i++) {
+		var foundIt = false;
+		//console.log("==================")
+		var tempArray = input.slice();
+		const thisOne = tempArray.splice(i, 1);
+		tempArray.some(function(item, index) {
+			//console.log("some loop: " + item)
+			if (item == thisOne) {
+				foundIt = true;
+				return true;
 			}
+			//return item === thisOne;
 		});
-	});
-	return returnThis;
+		if (!foundIt) {
+			//console.log("should add: " + thisOne);
+			//console.log("return array: " + returnThis.toString());
+			returnThis.length == 0 ? returnThis.push(thisOne[0]) : assert(false, "Invalid Input: Array has more than one singleton.");
+		}
+	}
+	return returnThis[0];
 }
 
 
 var methods = [findSingletonBrutish];
-var inputs = [[tBase, aBase], [tUnordered, aUnordered], [tDuplicate, aDuplicate], [tTypes, aTypes]];
+var inputs = [[tBase, aBase], [tUnordered, aUnordered], [tTypes, aTypes], [tDuplicate, "error"]];
 
 methods.forEach(function(method) {
-	console.log("Testing " + str(method) + "-------------------");
+	console.log("Testing " + method.name + " -------------------");
 	inputs.forEach(function(answerPair){
-		console.log("Testing with" + method + " with " + answerPair[0]);
-		assert(method(answerPair[0]) === answerPair[1]);
+		var answer = method(answerPair[0]);
+		console.log("Tested " + method.name + " with " + answerPair[0] + " -> " + answer.toString());
+		assert(answer == answerPair[1], typeof(answer) + " " + answer.toString() + " is not " + typeof(answerPair[1]) + " " + answerPair[1].toString());
 	});
 });
