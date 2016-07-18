5a7d2699887e05bfb98113220a45c98472ac6bd7
Ben Harris <bdharris08@gmail.com>
2016-07-18 14:00:34 -0500

    find the singleton with a map
    - changed thisOne to a var rather than const because apparently const
    persists through new loop scope in brutish
    - implemented function findSingletonLookup that uses a map and linear
    time

--- js/app/puzzles/singleton.js
+++ js/app/puzzles/singleton.js
@@ -18,6 +18,7 @@ const aUnordered = 1;
 const tTypes = [0, 'a', 'a', 0, "string"];
 const aTypes =  "string";
 const tDuplicate = [0,1,1,2,2,3];//exception
+const aDuplicate = "exception"
 
 //loops through each element, checking that element against the input with itself removed in order
 //worst case O(n^2)
@@ -25,11 +26,11 @@ function findSingletonBrutish (input) {
 	var returnThis = [];
 	for (var i = 0; i < input.length; i++) {
 		var foundIt = false;
-		//console.log("==================")
+		//console.log("================== " + i + " =======")
 		var tempArray = input.slice();
-		const thisOne = tempArray.splice(i, 1);
+		var thisOne = tempArray.splice(i, 1);
 		tempArray.some(function(item, index) {
-			//console.log("some loop: " + item)
+			//console.log("some loop: " + item + " compared to: " + thisOne)
 			if (item == thisOne) {
 				foundIt = true;
 				return true;
@@ -39,21 +40,39 @@ function findSingletonBrutish (input) {
 		if (!foundIt) {
 			//console.log("should add: " + thisOne);
 			//console.log("return array: " + returnThis.toString());
-			returnThis.length == 0 ? returnThis.push(thisOne[0]) : assert(false, "Invalid Input: Array has more than one singleton.");
+			returnThis.length == 0 ? returnThis.push(thisOne[0]) : returnThis.fill("exception");
 		}
 	}
 	return returnThis[0];
 }
 
 
-var methods = [findSingletonBrutish];
-var inputs = [[tBase, aBase], [tUnordered, aUnordered], [tTypes, aTypes], [tDuplicate, "error"]];
+//loop through the input, put stuff in a map
+//stack overflow says we can treat map inserts and lookups as "effectively" constant time
+//so worst case is O(2n) ~ O(n)
+function findSingletonLookup (input) {
+	var returnThis = [];
+	var counts = new Map();
+	input.forEach(function(item) {
+		counts.has(item) ? counts.set(item, counts.get(item) + 1) : counts.set(item, 1);
+	});
+	counts.forEach(function(value, key) {
+		if (value == 1) {
+			returnThis.length == 0 ? returnThis.push(key) : returnThis.fill("exception"); 
+		}
+	});
+	return returnThis[0];
+}
+
+
+var methods = [findSingletonBrutish, findSingletonLookup];
+var inputs = [[tBase, aBase], [tUnordered, aUnordered], [tTypes, aTypes], [tDuplicate, aDuplicate]];
 
 methods.forEach(function(method) {
 	console.log("Testing " + method.name + " -------------------");
 	inputs.forEach(function(answerPair){
 		var answer = method(answerPair[0]);
-		console.log("Tested " + method.name + " with " + answerPair[0] + " -> " + answer.toString());
-		assert(answer == answerPair[1], typeof(answer) + " " + answer.toString() + " is not " + typeof(answerPair[1]) + " " + answerPair[1].toString());
+		console.log("Tested " + method.name + " with " + answerPair[0] + " -> " + answer);
+		assert(answer == answerPair[1], typeof(answer) + " " + answer + " is not " + typeof(answerPair[1]) + " " + answerPair[1]);
 	});
 });
