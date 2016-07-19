d6104839aa8bdc6679a311bb7a9d8470201ad7f9
Ben Harris <bdharris08@gmail.com>
2016-07-19 13:54:33 -0500

    prepare string for palindrome check
    strip all the characters we don't want out and whitespace, normalize
    case, and return. Also have some asserts to test it

--- js/app/puzzles/palindrome.js
+++ js/app/puzzles/palindrome.js
@@ -22,7 +22,25 @@ testCases.set("l,o.l ; o! l!?", true);
 
 var ignoreThese = [" ", ",", ";", ".", "?", "!"];
 
+
+//strip characterSet from string and normalize case
+function prepare(string, characterSet) {
+  var strippedCharacters = new Set(characterSet);
+  var returnString = "";
+  for (var character of string) {
+    if (!strippedCharacters.has(character)) {
+      returnString = returnString.concat(character);
+    }
+  }
+  return returnString.toLowerCase();
+}
+
+
+
 var methods = [];
+
+assert(prepare(" aha ", ignoreThese) == "aha", "_aha_ was not stripped to aha");
+assert(prepare("A man. a plan; a canal, Panama?!", ignoreThese) == "amanaplanacanalpanama", "panama string was not stripped");
 
 testCases.forEach(function(value, key) {
   methods.forEach(function(method) {
