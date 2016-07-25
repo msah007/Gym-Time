311a6270948c8abc302a732572fd29afbcd43161
Ben Harris <bdharris08@gmail.com>
2016-07-25 13:50:45 -0500

    Test Traverse the BST and return a sorted string
    Created tests for a new function to traverse and print the string in
    order

--- js/app/structures/bst.js
+++ js/app/structures/bst.js
@@ -74,14 +74,22 @@ Node.prototype.insert = function(newValue) {
   }
 };
 
+var traverse = function(node) {
+  return "";
+}
+
 var testNode = new Node(2);
 assert(testNode.value === 2, "Node constructor didn't work");
+assert(traverse(testNode) == "2", "traverse didn't even return root");
 testNode.updateLeft(1);
 assert(testNode.left.value === 1, "Node updateLeft didn't work");
+assert(traverse(testNode) == "1 2", "traverse didn't return 1 2");
 testNode.updateRight(3);
 assert(testNode.right.value === 3, "Node updateRight didn't work");
+assert(traverse(testNode) == "1 2 3", "traverse didn't return 1 2 3");
 testNode.right.updateRight(4);
-assert(testNode.right.right.value === 4, "Updating the child's left didn't work")
+assert(testNode.right.right.value === 4, "Updating the child's left didn't work");
+assert(traverse(testNode) == "1 2 3 4", "traverse didn't return 1 2 3 4");
 assert(testNode.has(1), "has didn't find 1 left branch");
 assert(testNode.has(2), "has didn't find 2 root node");
 assert(testNode.has(3), "has didn't find 3 right branch");
@@ -91,6 +99,8 @@ assert(testNode.search(1) == testNode.left, "search didn't find 1 left branch");
 assert(testNode.search(3) == testNode.right, "search didn't find 3 right branch");
 testNode.insert(5);
 assert(testNode.has(5), "has didn't find the insert(5)");
+//console.log(traverse(testNode));
+assert(traverse(testNode) == "1 2 3 4 5", "traverse didn't return 1 2 3 4 5");
 
 var tree = new Node(6);
 assert(tree.insert(3), "tree insert(3) failed");
@@ -100,3 +110,5 @@ assert(tree.has(6), "has didn't find root 6 in tree");
 assert(tree.has(3), "has didn't find root 3 in tree");
 assert(tree.has(4), "has didn't find root 4 in tree");
 assert(tree.has(5), "has didn't find root 5 in tree");
+//console.log(traverse(tree))
+assert(traverse(tree) == "3 4 5 6")
